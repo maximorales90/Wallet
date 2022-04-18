5b187a8f54894a635f2443d5e89a14f21ec1c9dc
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-04-18 13:06:12 -0300

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Maximiliano Morales on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
